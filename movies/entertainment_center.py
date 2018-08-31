cea746a0dc1c89a3a19259633be8c90d9332e002
Ding Liu <movingon.ding@gmail.com>
2018-08-30 17:24:31 -0700

    movie website to display poster and trailer

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
@@ -6,4 +7,17 @@ toy_story = media.Movie("Toy Story",
                         "https://www.youtube.com/watch?v=tN1A2mVnrOM")
 
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
+
+#toy_story.show_trailer()
+
+island = media.Movie("Island",
+                     "A cataclysmic event causes a man, who dreams of winning the lottery, to become stranded on an island with his co-workers.",
+                     "http://www.goldposter.com/zh/273490/",
+                     "https://www.youtube.com/watch?v=0xSl0Dsmw_0")
+
+#island.show_trailer()
+
+movies = [toy_story, island]
+
+fresh_tomatoes.open_movies_page(movies)
